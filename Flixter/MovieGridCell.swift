e531c53c61032bc8c983f0f8ad989b2c9d2b1826
omarkhan5299 <omar.5299@gmail.com>
2022-03-01 13:11:46 -0500

    SecondAssignment

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Omar Khan  on 3/1/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
